2d729ac92c670cca2d6877c18a961a8fcf9b701e
Daniil <113169271+Yortemq@users.noreply.github.com>
2022-10-03 18:44:00 +0300

    Add files via upload

---
+++ Lab2_2_23.09/Lab2_2_23.09/Lab2_2_23.09.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include "Header.h"
+using std::cin;
+using std::endl;
+int num(int y, int y1);
+
+int main()
+{
+	int y = 10;
+	int y1 = 15;
+	int t = num(y, y1);
+	int u = num1(y, y1);
+	cin.get();
+}
+
